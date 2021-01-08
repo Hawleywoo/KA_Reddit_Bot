3daa5208495b2b83d1f8e8dbb1e49f71fcb501e5
Hawleywoo <alhawley21@gmail.com>
2021-01-08 16:20:04 -0700

    Write to reddit

--- red_bot.py
+++ red_bot.py
@@ -1,3 +1,26 @@
 import praw
+import pdb
+import re
+import os
 
-print( praw.__path__)
+reddit = praw.Reddit('KAbot', user_agent='KAbot 0.1')
+subreddit = reddit.subreddit('pythonforengineers')
+
+#     print('Title', submissions.title)
+#     print('selftext', submissions.selftext)
+#     print('score', submissions.score)
+#     print('\n \n \n \n \n')
+
+if not os.path.isfile('posts_replied_to.txt'):
+    posts_replied_to = []
+else: 
+    with open('posts_replied_to.txt', 'r') as f:
+        post_replied_to = f.read()
+        post_replied_to = post_replied_to.split('\n') 
+        post_replied_to = list(filter(None, post_replied_to))
+    
+for submission in subreddit.hot(limit=5):
+    if submission.id not in posts_replied_to:
+        if re.search('i love python', submission.title, re.IGNORECASE):
+            submission.reply('Botty bot says: Me Too!')
+
